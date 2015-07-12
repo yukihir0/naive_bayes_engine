6d782386b2b0684afb3c221351339e86a8a6a7fd
yukihir0 <yukihiro.cotori@gmail.com>
2015-07-12 10:10:02 +0900

    add coveralls setting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,6 @@
 # coding: utf-8
+require 'coveralls'
+Coveralls.wear!
 require 'naive_bayes_engine'
 require 'naive_bayes_engine/algorithm/naive_bayes'
 require 'naive_bayes_engine/algorithm/complemental_naive_bayes'
